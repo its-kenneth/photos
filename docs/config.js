fc1963044cd8902490e418ef0d766b4a32de4ad5
kenneth <88260663+its-kenneth@users.noreply.github.com>
2026-08-11 12:44:35 +0800

    Update GITHUB_TOKEN in config.js

--- docs/config.js
+++ docs/config.js
@@ -6,6 +6,6 @@
 // views this page can read and reuse it to write to GITHUB_REPO below.
 // Do not scope it to anything beyond this one throwaway storage repo.
 
-const GITHUB_TOKEN = "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";
+const GITHUB_TOKEN = "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";
 const GITHUB_OWNER = "its-kenneth";
 const GITHUB_REPO = "photos";
